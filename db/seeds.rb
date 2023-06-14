aab9068b6c30895750719b8f4f6d31296d2a121e
Karen Ventura <karen.ventura@housecallpro.com>
2023-06-14 16:19:20 -0600

    Feat: First commit

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+user = User.new
+user.password = '1234566'
+user.name = 'Karen Ventura'
+user.email = 'karen@hcp.com'
+user.save!
+
+user.update(password: Faker::Internet.password)
+(1..10).each do |_index|
+  user = User.new
+  user.password = Faker::Internet.password
+  user.name = Faker::Name.name
+  user.email = Faker::Internet.email
+  user.save!
+end
